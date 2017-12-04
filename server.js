2a14fbba5a94af377250dc291e513864e1a92189
Andre Lademann <andre.lademann@netresearch.de>
2017-12-04 18:28:47 +0100

    Set internal port to 80

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 let express = require('express'),
     app = express(),
     morgan = require('morgan'),
-    port = process.env.PORT || 3000,
+    port = process.env.PORT || 80,
     host = process.env.HOST || '0.0.0.0';
 
 app.use(morgan('combined'));
